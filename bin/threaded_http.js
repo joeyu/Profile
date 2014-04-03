78096c0b0d40277774865194441b8cd0addff41b
Zhou Yu <nap.zhou.yu@gmail.com>
2014-04-03 15:18:44 +0800

    Added tget and tuned threaded_http

--- bin/threaded_http.js
+++ bin/threaded_http.js
@@ -15,39 +15,32 @@ function ThreadedGetter(srcUrl, destPath, nThreads, md5sum, callback) {
     this.destPath = destPath;
     this.nThreads = nThreads;
     this.md5sum = md5sum;
-
     this.isThreadable = true; // does server support multi-threaded GET?
     this.size = 0; // file size
     this.sizeGot = 0; // file size got
     this.threads = []; 
-
-    var urlObj = url.parse(srcUrl);
-    var options = {
-          hostname: urlObj.hostname,
-          port:urlObj.port, 
-          path: urlObj.path,
-          method: 'HEAD'
-    };
+    this.end = false;
     this.callback = callback;
 
     self = this;
-    self.on('thread_data', function (i, chunk) {
+    self.on('_thread_data', function (i, chunk) {
         var t = self.threads[i];
+        var sizeGot = chunk.length;
         t.bufs.push(chunk);
-        t.sizeGot += chunk.length; 
-        self.sizeGot += chunk.length;
+        t.sizeGot += sizeGot;
+        self.sizeGot += sizeGot;
         //console.log("chunk: size: %d", t.size);
-    }).on('thread_finished', function (i) {
+        self.emit('thread_data', i, chunk);
+    }).on('_thread_end', function (i) {
         var t = self.threads[i];
         t.finished = true;
         self.nThreads --;
-
         if (areAllThreadsFinished()) {
-            self.emit('all_threads_finished');
+            self.emit('_end');
         }
-
         //console.log(t);
-    }).on('all_threads_finished', function () {
+        self.emit('thread_finished', i);
+    }).on('_end', function () {
         var bufs = self.threads.map(function (e) {
             var buf = Buffer.concat(e.bufs, e.sizeGot);
             return buf;
@@ -57,69 +50,127 @@ function ThreadedGetter(srcUrl, destPath, nThreads, md5sum, callback) {
         var fd = fs.createWriteStream(destPath);
         fd.write(bufFile);
         fd.end();
+        self.end = true;
     });
 
     self.callback(self);
 
+    var urlObj = url.parse(self.url);
+    var options = {
+          hostname: urlObj.hostname,
+          port:urlObj.port, 
+          path: urlObj.path,
+          method: 'HEAD'
+    };
     http.request(options, function(res) {
         //console.log('STATUS: ' + res.statusCode);
         //console.log('HEADERS: ' + util.inspect(res.headers, {colors: true}));
         if (res.statusCode != 200) {
-            console.log("[ERROR] HTTP 'HEAD' error code: " + res.statusCode);
-            return res.statusCode;
-        }
-        if (res.headers['accept-ranges'] !== 'bytes') {
-            console.log("[WARNING] HTTP partial 'GET' is not supported! " + util.inspect(res.headers, {colors: true}));
-            this.isThreadadle = false;
-        }
-        self.size  = res.headers['content-length'];
-
-        // calculate the range for each thread 
-        var start = 0;
-        var step = self.size / self.nThreads;
-        var last = self.size % self.nThreads;
-        if (last) {
-            step = parseInt(step) + 1;
-        }
-        
-        for (var i = 0; start + step <= self.size; ++ i) {
-            self.threads.push(
-                {
-                    bufs: [], 
-                    start: start,
-                    end: start + step - 1,
-                    sizeGot: 0, 
-                    finised: false
-                });
-            getPartialFile(i, start, start + step - 1);
-            start += step;
-        }
-        if (last) {
-            getPartialFile(i, start, self.size - 1);
-        }
-
-        // Start
-        function getPartialFile (i, start, end) {
-            options.method = 'GET';
-            var req = http.request(options, function(res) {
-                if (res.statusCode != 206) {
-                    console.log("[ERROR] HTTP partial 'GET' failed! " + util.inspect(res.headers, {colors: true}));
-                    exit(1); 
+            if (res.statusCode == 302 || res.statusCode == 303) {
+                self.url = res.headers['location'];
+                console.log(["[INFO] Redirected to %s", self.url);
+                var urlDirectObj = url.parse(self.url);
+                var options = {
+                      hostname: urlDirectObj.hostname,
+                      port:urlDirectObj.port, 
+                      path: urlDirectObj.path,
+                      method: 'HEAD'
                 }
-                // just send events
-                res.on('data', function (chunk) {
-                    self.emit('thread_data', i, chunk);
-                }).on('end', function() {
-                    self.emit('thread_finished', i);
-                });
-            });
-            req.setHeader('range', 'bytes=' + start + '-' + end);
-            req.end();
+                http.request(options, function (res) {
+                    response(res);
+                }).on('error', function (e) {
+                    console.log("[ERROR] Problem with http 'GET' request: " + e.message);
+                    return res.statusCode;
+                }).end();
+            }
+            else {
+                console.log("[ERROR] HTTP 'HEAD' error code: %d", res.statusCode);
+                return res.statusCode;
+            }
         }
-        options.method = 'GET';
+        else {
+            response(res);
+        }
+
+        function response (res) {
+            if (res.headers['accept-ranges'] !== 'bytes') {
+                console.log("[WARNING] HTTP partial 'GET' is not supported! " + util.inspect(res.headers, {colors: true}));
+                this.isThreadadle = false;
+            }
+
+            self.size  = res.headers['content-length'];
+
+            // calculate the range for each thread 
+            var start = 0;
+            var step = self.size / self.nThreads;
+            var last = self.size % self.nThreads;
+            if (last) {
+                step = parseInt(step) + 1;
+            }
+            
+            for (var i = 0; start + step <= self.size; ++ i) {
+                self.threads.push(
+                    {
+                        bufs: [], 
+                        start: start,
+                        end: start + step - 1,
+                        sizeGot: 0, 
+                        finised: false
+                    });
+                getPartialFile(i, start, start + step - 1);
+                start += step;
+            }
+            if (last) {
+                self.threads.push(
+                    {
+                        bufs: [], 
+                        start: start,
+                        end: self.size - 1,
+                        sizeGot: 0, 
+                        finised: false
+                    });
+                getPartialFile(i, start, self.size - 1);
+            }
+
+            // Start
+            function getPartialFile (i, start, end) {
+                var urlObj = url.parse(self.url);
+                var options = {
+                      hostname: urlObj.hostname,
+                      port:urlObj.port, 
+                      path: urlObj.path,
+                      method: 'GET'
+                };
+                var req = http.request(options, function(res) {
+                    if (res.statusCode != 206) {
+                        console.log("[ERROR] HTTP partial 'GET' failed! " + util.inspect(res.headers, {colors: true}));
+                        exit(1); 
+                    }
+                    // just send events
+                    res.on('data', function (chunk) {
+                        self.emit('_thread_data', i, chunk);
+                    }).on('end', function() {
+                        self.emit('_thread_end', i);
+                    });
+                });
+                req.setHeader('range', 'bytes=' + start + '-' + end);
+                req.on('error', function(e) {
+                    console.log("[ERROR] Problem with http 'GET' request: " + e.message);
+                }).end();
+            }
+        }
     }).on('error', function(e) {
         console.log("[ERROR] Problem with http 'HEAD' request: " + e.message);
     }).end();
+
+    (function () {
+        if (self.end) {
+            self.emit('end');
+        }
+        else {
+            setTimeout(arguments.callee, 1000);
+        }
+    })();
 
     function areAllThreadsFinished() {
         return self.threads.every(function (e) {return e.finished;});
