18b955af974895d7d61a199ef4df4afd4ba7009c
Zhou Yu <nap.zhou.yu@gmail.com>
2019-05-01 14:06:07 +0800

    Updated vresize to support x265

--- bin/vresize.py
+++ bin/vresize.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
 Created on Fri Mar 29 11:01:27 2019
@@ -9,40 +10,57 @@ import sys
 import subprocess
 
 
-def convert(infile, target_size):
-    infile = Path(infile)
+def convert(infile_str, target_size, codec):
+    infile = Path(infile_str)
     infile_size = infile.stat().st_size
-    targetfile = infile.stem + '2' + infile.suffix
-    cmd = f"ffprobe -v error -select_streams v:0 -show_entries stream=bit_rate -of default=noprint_wrappers=1:nokey=1 {infile}".split()
-    infile_bv = int(subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True).stdout.rstrip('\n'))
-    cmd = f"ffprobe -v error -select_streams a:0 -show_entries stream=bit_rate -of default=noprint_wrappers=1:nokey=1 {infile}".split()
-    infile_ba = int(subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True).stdout.rstrip('\n'))
-    target_ba = infile_ba
-    if target_ba > 6.4e4:
-         target_ba = int(6.4e4)
-    target_bv = target_size * (infile_ba + infile_bv) // infile_size - target_ba
+    if infile.suffix == ".mp4":
+        targetfile = infile.stem + '2' + infile.suffix
+        cmd = f"ffprobe -v error -select_streams v:0 -show_entries stream=bit_rate -of default=noprint_wrappers=1:nokey=1".split()
+        cmd.append({infile_str})
+        infile_bv = int(subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True).stdout.rstrip('\n'))
+        cmd = f"ffprobe -v error -select_streams a:0 -show_entries stream=bit_rate -of default=noprint_wrappers=1:nokey=1".split()
+        cmd.append({infile_str})
+        infile_ba = int(subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True).stdout.rstrip('\n'))
+        target_ba = infile_ba
+        if target_ba > 6.4e4:
+             target_ba = int(6.4e4)
+        target_bv = target_size * (infile_ba + infile_bv) // infile_size - target_ba
+    elif infile.suffix == ".webm":
+        targetfile = infile.stem + '2.mp4'
+        cmd = f"ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1".split()
+        cmd.append(infile_str)
+        duration = float(subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True).stdout.rstrip('\n'))
+        duration = int(duration)
+        #print(f"duration: {duration}")
+        target_ba = int(6.4e4)
+        target_bv = target_size * 8 // duration - target_ba
     #print(f"infile_size: {infile_size}")
     #print(f"target_size: {target_size}")
     #print(f"infile_ba: {infile_ba}")
     #print(f"infile_bv: {infile_bv}")
     #print(f"target_ba: {target_ba}")
     #print(f"target_bv: {target_bv}")
-    cmd = f"ffmpeg -i {infile} -c:v libx264 -b:v {target_bv} -pass 1 -f mp4 -an /dev/null -y".split()
+    #return
+    codec_pass1 = "-x265-params pass=1:pools=4"
+    codec_pass2 = "-x265-params pass=2:pools=4"
+    if codec is "libx264":
+        codec_pass1 = "-pass 1"
+        codec_pass2 = "-pass 2"
+    cmd = f"ffmpeg -i".split() + [infile_str] + f"-threads 4 -c:v {codec} -b:v {target_bv} {codec_pass1} -f mp4 -an /dev/null -y".split()
     subprocess.run(cmd, check = True)
-    cmd = f"ffmpeg -i {infile} -c:v libx264 -b:v {target_bv} -pass 2 -b:a {target_ba} {targetfile} -y".split()
+    cmd = f"ffmpeg -i".split() + [infile_str] + f"-threads 4 -c:v {codec} -b:v {target_bv} {codec_pass2} -b:a {target_ba} -y".split() + [str(targetfile)]
     subprocess.run(cmd, check = True)
 
-if len(sys.argv) <= 3:
+if len(sys.argv) <= 4:
     infile = sys.argv[1] 
-    if len(sys.argv) == 3:
-        target_size = sys.argv[2]
-        if target_size[-1] == 'M' or target_size[-1] == 'm':
-            target_size = int(target_size[0:-1]) * 1e6
-        else:
-            raise NotImplementedError
-    else:
-        target_size = 2.45e7
+    codec = "libx264"
+    target_size = 2.46e7
+    for arg in sys.argv[2:]:
+        if arg in ("libx265", "libx264"):
+            codec = arg
+        elif arg[-1] in ("M", "m"):
+            target_size = int(arg[0:-1]) * 1e6
 else:
-    print(f"Usage: {sys.argv[0]} <infile> [target_size]")
+    print(f"Usage: {sys.argv[0]} <infile> [target_size] [codec]")
 
-convert(infile, target_size)
+convert(infile, target_size, codec)
